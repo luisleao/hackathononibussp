77aafc74bf8585b94285a231c48fd5e9b92219a3
Luis Leao <luis.leao@gmail.com>
2013-10-26 14:42:05 -0200

    interface + scripts

--- web/js/main.js
+++ web/js/main.js
@@ -5,19 +5,20 @@ var map;
 
 var initializeMap = function() {
 
-	var brazil = new google.maps.LatLng(-23.576732121654487, -46.633530120117186);
+	var saopaulo = new google.maps.LatLng(-23.576732121654487, -46.633530120117186);
 	var mapOptions = {
 		zoom: 12,
-		center: brazil,
-		mapTypeControl: true,
+		center: saopaulo,
+		overviewMapControl: false,
+		zoomControlOptions: { style: google.maps.ZoomControlStyle.SMALL },
 		panControl: false,
-		scaleControl: false,
 		streetViewControl: false,
-		zoomControl: true,
+		mapTypeControl: false,
+
 		//mapTypeControlOptions: { mapTypeIds: [MY_MAPTYPE_ID]},
 		//mapTypeId: MY_MAPTYPE_ID,
-		navigationControlOptions: { style: google.maps.NavigationControlStyle.SMALL },
-
+		//scaleControl: false,
+		//zoomControl: true,
 	}
 
 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
@@ -35,6 +36,9 @@ jQuery(document).ready(function($) {
 	console.log("init");
 	google.maps.event.addDomListener(window, 'load', initializeMap);
 	
+
+	$(".modal").modal({show: false});
+
 
 
 });
